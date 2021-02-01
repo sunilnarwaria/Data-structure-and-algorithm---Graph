c49e3b7d320d0ca4c0612d53f24e049f65bffcba
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-02-01 08:25:14 +0530

    Update is graph bipartite.cpp

--- is graph bipartite.cpp
+++ is graph bipartite.cpp
@@ -43,3 +43,32 @@
                 ans = ans & solve(graph,col,i,vis);
             }
          }
+      
+      // using DFS method
+      
+      
+       bool dfs(vector<vector<int>> & graph,vector<int> &color,int u)
+    {
+        for(auto v:graph[u])
+        {
+            if(color[v]==-1)
+            {
+                color[v]=1-color[u];
+                if(!dfs(graph,color,v)) return false;
+            }
+            else if(color[v]==color[u]) return false;
+        }
+	    return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V=graph.size();
+        vector<int> color(V,-1);
+	    for(int i=0;i<V;i++)
+	        if(color[i]==-1) 
+	        {
+	            color[i]=1;
+	            if(!dfs(graph,color,i))
+	            return false;
+	        }
+	    return true;
+    }
