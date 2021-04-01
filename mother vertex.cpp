d45cae1c4df1a3f8884302bb85077a582cba4034
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-04-01 16:36:32 +0530

    Create mother vertex.cpp

---
+++ mother vertex.cpp
@@ -0,0 +1,2 @@
+// dfs lgao random order mi kie bhi vertex se jb backtrck k rhe hoge us time pr tack mai node  push krte jao.
+//jb stck size totl number of node ho jaye tb top of the stack is mother vertex 
